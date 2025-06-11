7dceac2f0b7cf12e8c20367aad0d854967cbce3d
Nadeem Samaali <nadeem.samaali@gmail.com>
2025-06-11 16:19:57 -0400

    Fetches submission README file

--- sync.py
+++ sync.py
@@ -3,6 +3,7 @@ import os
 import tempfile
 from pathlib import Path
 from git import Repo
+import html2text
 
 # Github user information
 GIT_PAT = os.getenv("GIT_PAT")
@@ -80,15 +81,46 @@ leetcode_lang_to_ext = {
     "assembly": ".asm"
 }
 
+def fetch_problem_readme(title_slug):
+    url = "https://leetcode.com/graphql"
+    query = {
+        "operationName": "questionData",
+        "variables": {"titleSlug": title_slug},
+        "query": """
+        query questionData($titleSlug: String!) {
+          question(titleSlug: $titleSlug) {
+            content
+            title
+            questionId
+            difficulty
+            likes
+            dislikes
+            exampleTestcases
+          }
+        }
+        """
+    }
+
+    response = requests.post(url, json=query, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        content_html = data["data"]["question"]["content"]
+        return content_html
+    else:
+        raise Exception(f"Failed to fetch problem: {response.status_code}\n{response.text}")
+
 def get_submission_metadata(sub) : 
-    dir_header = f'{sub["question_id"]}-{sub["title_slug"]}/{sub["title_slug"]}-{sub["timestamp"]}{leetcode_lang_to_ext[sub["lang"]]}'
+    id = str(sub["question_id"]).zfill(4)
+    dir_header = f'{id}-{sub["title_slug"]}/{sub["title_slug"]}-{sub["timestamp"]}{leetcode_lang_to_ext[sub["lang"]]}'
     commit_message = f'Runtime : {sub["runtime"]} | Memory : {sub["memory"]}'
     code = sub["code"]
-    id = int(sub["question_id"])
+    readme = html2text.html2text(fetch_problem_readme(sub["title_slug"]))
 
     return {"id": id,
             "dir" : dir_header,
             "code" : code,
+            "readme" : readme,
             "commit_message" : commit_message
             }
 
@@ -120,20 +152,29 @@ while has_next:
     offset += limit
 
 fetched_submissions = sorted(fetched_submissions, key=lambda x: x["id"])
-print(fetched_submissions)
 
 # Commiting each file separately         
 for sub in fetched_submissions :
     rel_path = sub["dir"]
     content = sub["code"]
+    readme = sub["readme"]
     commit_message = sub["commit_message"]
 
-    file_path = temp_path / rel_path
-    file_path.parent.mkdir(parents=True, exist_ok=True)
-    file_path.write_text(content, encoding="utf-8")
+    file_path_submission = temp_path / rel_path
+    file_path_readme = file_path_submission.parent / "README.md"
+
+    file_path_submission.parent.mkdir(parents=True, exist_ok=True)
+    file_path_submission.write_text(content, encoding="utf-8")
+
+    file_path_readme.parent.mkdir(parents=True, exist_ok=True)
+    file_path_readme.write_text(readme, encoding="utf-8")
 
     repo.git.add(rel_path)
     repo.index.commit(commit_message)
+    repo.git.add(str(file_path_readme.relative_to(temp_path)))
+    repo.index.commit("Added submission README file")
+
+
     # Push
     repo.remote().push()
 
