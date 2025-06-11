b90771df6daec0687585697232231258d486a3e8
Nadeem Samaali <nadeem.samaali@gmail.com>
2025-06-11 11:20:10 -0400

    Fully functionning prototype : Fetches all accepted submissions and pushes to github

--- sync.py
+++ sync.py
@@ -1,12 +1,35 @@
 import requests
 import os
 import tempfile
+from pathlib import Path
+from git import Repo
+
+# Github user information
+GIT_PAT = os.getenv("GIT_PAT")
+REPO_URL = os.getenv("REPO_URL")
+
+repo_name = REPO_URL.split("/")[-1]
+repo_owner = REPO_URL.split("/")[-2]
+
+git_url = f'https://{GIT_PAT}:x-oauth-basic@github.com/{repo_owner}/{repo_name}'
+
+# Clone repo into a temporary directory
+temp_dir = tempfile.TemporaryDirectory()
+temp_path = Path(temp_dir.name)
+repo = Repo.clone_from(git_url, temp_path)
+
+# Read repo files into memory
+file_map = {}
+for file in temp_path.rglob("*") :
+    if file.is_file() and ".git" not in file.parts:
+        rel_path = str(file.relative_to(temp_path))
+        file_map[rel_path] = file.read_text(encoding="utf8")
 
 # Leetcode user information
 LEETCODE_SESSION = os.getenv("LEETCODE_SESSION")
 CSRFTOKEN = os.getenv("CSRFTOKEN")
 
-# Headers
+# Leetcode Headers
 headers = {
     "cookie": f"LEETCODE_SESSION={LEETCODE_SESSION}; csrftoken={CSRFTOKEN};",
     "x-csrftoken": CSRFTOKEN,
@@ -16,20 +39,57 @@ headers = {
 
 # User proxies if necessary
 proxies = {
-    "http":"",
-    "https":"",
+    "http": os.getenv("HTTP_PROXY"),
+    "https": os.getenv("HTTPS_PROXY"),
+}
+
+leetcode_lang_to_ext = {
+    "cpp": ".cpp",
+    "java": ".java",
+    "python": ".py",
+    "python3": ".py",
+    "c": ".c",
+    "csharp": ".cs",
+    "javascript": ".js",
+    "typescript": ".ts",
+    "php": ".php",
+    "swift": ".swift",
+    "kotlin": ".kt",
+    "dart": ".dart",
+    "golang": ".go",
+    "scala": ".scala",
+    "rust": ".rs",
+    "ruby": ".rb",
+    "mysql": ".sql",
+    "mssql": ".sql",
+    "oraclesql": ".sql",
+    "bash": ".sh",
+    "racket": ".rkt",
+    "erlang": ".erl",
+    "elixir": ".ex",
+    "haskell": ".hs",
+    "perl": ".pl",
+    "lua": ".lua",
+    "clojure": ".clj",
+    "fsharp": ".fs",
+    "vb": ".vb",
+    "julia": ".jl",
+    "scheme": ".scm",
+    "groovy": ".groovy",
+    "pascal": ".pas",
+    "assembly": ".asm"
 }
 
 def get_submission_metadata(sub) : 
-    dir_header = f'{sub["question_id"]}-{sub["title_slug"]}'
+    dir_header = f'{sub["question_id"]}-{sub["title_slug"]}/{sub["title_slug"]}-{sub["timestamp"]}{leetcode_lang_to_ext[sub["lang"]]}'
     commit_message = f'Runtime : {sub["runtime"]} | Memory : {sub["memory"]}'
-    language = sub["lang_name"]
     code = sub["code"]
+    id = int(sub["question_id"])
 
-    return {"dir_header" : dir_header,
+    return {"id": id,
+            "dir" : dir_header,
             "code" : code,
-            "commit_message" : commit_message,
-            "language" : language
+            "commit_message" : commit_message
             }
 
 # Pagination
@@ -38,15 +98,13 @@ limit = 10
 has_next = True
 fetched_submissions = []
 
-seen_problems = set()  # keep track of problems with accepted submissions found
+#seen_problems = set()  # keep track of problems with accepted submissions found
 
 while has_next:
     url = f"https://leetcode.com/api/submissions/?offset={offset}&limit={limit}"
     response = requests.get(url, headers=headers, proxies=proxies)
 
     if response.status_code != 200:
-        print("Failed to fetch submissions:", response.status_code)
-        print(response.text)
         continue
 
     data = response.json()
@@ -54,23 +112,31 @@ while has_next:
 
     for sub in submissions:
         slug = sub["title_slug"]
-        if sub["status_display"] == "Accepted" and slug not in seen_problems:
-            seen_problems.add(slug)
-
+        if sub["status_display"] == "Accepted" : #and slug not in seen_problems:
+            #seen_problems.add(slug)
             fetched_submissions.append(get_submission_metadata(sub))
 
     has_next = data.get("has_next", False)
     offset += limit
 
-class git_handler : 
+fetched_submissions = sorted(fetched_submissions, key=lambda x: x["id"])
+print(fetched_submissions)
 
-    def __init__(self) :
-        # Github user information
-        GIT_PAT = os.getenv("GIT_PAT")
-        REPO_URL = os.getenv("REPO_URL")
+# Commiting each file separately         
+for sub in fetched_submissions :
+    rel_path = sub["dir"]
+    content = sub["code"]
+    commit_message = sub["commit_message"]
 
-        repo_name = REPO_URL.split("/")[-1]
-        repo_owner = REPO_URL.split("/")[-2]
+    file_path = temp_path / rel_path
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+    file_path.write_text(content, encoding="utf-8")
 
-        git_url = f'https://{GIT_PAT}:x-oauth-basic@github.com/{repo_owner}/{repo_name}.git'        
+    repo.git.add(rel_path)
+    repo.index.commit(commit_message)
+    # Push
+    repo.remote().push()
 
+# Clean up locks to avoid PermissionError
+repo.close()
+temp_dir.cleanup()
